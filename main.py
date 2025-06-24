15e5c636dd55e290c079fe694ac289eff21985a0
Koro <120288655+Korotime@users.noreply.github.com>
2025-06-24 10:03:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 df_vn = pd.read_excel("trans_vn_cards.xlsx").fillna("")
-print("DEBUG tên nhận:", self.card_name)
-print("DEBUG danh sách df_vn:", df_vn['name'].str.lower().tolist())
 
 intents = discord.Intents.default()
 intents.message_content = True
